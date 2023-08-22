4e8db772a17ccb505e62455e405009966afcceb0
Zzcopo <108587894+Zzcopo@users.noreply.github.com>
2023-08-22 14:25:24 -0300

    Update IfeElse16.java

--- IfeElse16.java
+++ IfeElse16.java
@@ -1,23 +1,22 @@
 import java.util.Scanner;
 
 public class IfeElse16 {
-
 	public static void main(String[] args) {
+
 		Scanner input = new Scanner(System.in);
-		System.out.println("quantas horas voce trabalhou nesse mes?");
-		double HoraspMes = input.nextDouble();
-		System.out.println("Quanto voce recebe por hora?");
-		double SalpHora = input.nextDouble();
-		double HoraEx = ((SalpHora * 50)/ 100);
-		double SalpMes = (SalpHora * 160);
-		double SalFin = (SalpMes + HoraEx );
-		if (HoraspMes / 4 > 40) {
-			System.out.println("Voce vai receber "+ HoraEx +" de hora extra, seu salario total sera de " + SalFin);
+		System.out.println("Quantas horas foram trabalhadas?");
+		double horasTrabalhadas = input.nextDouble();
+		System.out.println("Qual seu salario por hora?");
+		double salarioHora = input.nextDouble();
+		
+		if(horasTrabalhadas > 160) {
+			double horaExtra = horasTrabalhadas - 160;
+			double salarioExtra = horaExtra * (salarioHora * 1.5);
+			System.out.println("Salario: " + ((salarioHora * 160) + salarioExtra));
+		} else {
+			System.out.println("Salario: " + salarioHora * 160);
 		}
-		
-		
-		
-	
+
 	}
 
 }
